e73b7a8b0e3368db58eaa5ecd1f85764c9227979
b3n10 <francisben.lleve@gmail.com>
2020-04-27 14:54:35 +0800

    Done: generating random incorrect answer.

--- app/src/main/java/com/newbie/android/the100game/Question.kt
+++ app/src/main/java/com/newbie/android/the100game/Question.kt
@@ -1,14 +1,44 @@
 package com.newbie.android.the100game
 
-data class Question(val mainNumber: Int = (Math.random() * 100).toInt()) {
+class Question {
+    val mainNumber: Int = randomNumber(100)
     val correctAnswer: Int = 100 - mainNumber
-    val correctButton: Int = (Math.random() * 10).toInt() % 4
 
-    val buttonLists = listOf<Int>(
-       // generateIncorrectAnswer()
-    )
+    val answers = mutableListOf<Int>().apply {
+        val correctButton: Int = randomNumber(10) % 4
 
-    private fun generateIncorrectAnswer() {
-       // generate an incorrect answer based on correctAnswer
+        for (i in 0..3) {
+            if (i == correctButton)
+                this.add(correctAnswer)
+            else
+                this.add(generateIncorrectAnswer())
+        }
     }
+
+    /*
+    private fun generateAnswers(): List<Int> {
+        val answers = mutableListOf<Int>()
+
+        for (i in 0..3) {
+            if (i == correctButton)
+                answers[i] = correctAnswer
+            else
+                answers[i] = generateIncorrectAnswer()
+        }
+
+        return answers
+    }
+     */
+
+    private fun generateIncorrectAnswer(): Int {
+        var answer = randomNumber(100)
+
+        while (answer == correctAnswer) {
+            answer = randomNumber(100)
+        }
+
+        return answer
+    }
+
+    private fun randomNumber(limit: Int) = (Math.random() * limit).toInt()
 }
