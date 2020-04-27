f7908e26f86f3399c7a5f86f270b5afab2fe7dca
b3n10 <francisben.lleve@gmail.com>
2020-04-27 15:49:21 +0800

    Bind checkAnswer function to each button. Update gradle to apply 'kotlin-kapt' plugin.

--- app/src/main/java/com/newbie/android/the100game/MainActivity.kt
+++ app/src/main/java/com/newbie/android/the100game/MainActivity.kt
@@ -2,6 +2,11 @@ package com.newbie.android.the100game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.view.forEach
 import androidx.databinding.DataBindingUtil
 import com.newbie.android.the100game.databinding.ActivityMainBinding
 
@@ -11,6 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.question = Question()
+        val question = Question()
+        binding.question = question
+
+        binding.linearLayoutContainer.forEach { button ->
+            button.setOnClickListener {
+                    view -> checkAnswer(view as Button, question)
+            }
+        }
+    }
+
+    private fun checkAnswer(button: Button, question: Question) {
+        val textButton = button.text
+        val textAnswer = question.correctAnswer.toString()
+
+        val text = if (textAnswer == textButton)
+            "Correct: button is $textButton, answer is $textAnswer"
+        else
+            "Incorrect: button is $textButton, answer is $textAnswer"
+
+        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.CENTER, 0, 0)
+            show()
+        }
     }
 }
