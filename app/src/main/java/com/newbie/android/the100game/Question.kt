713bcb13d143b3257dca6dffc1f6f08f58b2f4bf
b3n10 <francisben.lleve@gmail.com>
2020-04-27 13:14:10 +0800

    Still thinking the logic to generate correct/incorrect answer.

--- app/src/main/java/com/newbie/android/the100game/Question.kt
+++ app/src/main/java/com/newbie/android/the100game/Question.kt
@@ -1,5 +1,14 @@
 package com.newbie.android.the100game
 
 data class Question(val mainNumber: Int = (Math.random() * 100).toInt()) {
-    private val correctAnswer: Int = 100 - mainNumber
+    val correctAnswer: Int = 100 - mainNumber
+    val correctButton: Int = (Math.random() * 10).toInt() % 4
+
+    val buttonLists = listOf<Int>(
+       // generateIncorrectAnswer()
+    )
+
+    private fun generateIncorrectAnswer() {
+       // generate an incorrect answer based on correctAnswer
+    }
 }
